e72ea3552d737cb93cf461fc015b8190c70e8cb2
Zach Coursey <zcoursey22@gmail.com>
2018-07-16 20:32:16 -0700

    Server serves static files

--- server.js
+++ server.js
@@ -2,4 +2,6 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.static('./client'));
+
 app.listen(port, console.log(`server running on port ${port}`));
